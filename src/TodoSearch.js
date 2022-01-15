3390021faca2474bf22524cd66088bb54da92d8c
Edison Panchi <edison@cladian.com>
2022-01-14 22:23:01 -0500

    Components
    Components creation and mapping

---
+++ src/TodoSearch.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TodoSearch(){
+    return(
+        <input placeholder="test" />
+    )
+}
+// export nombrados, obligamos a nombrarlo de esta manera
+export { TodoSearch }
