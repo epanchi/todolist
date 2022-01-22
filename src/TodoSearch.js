bf1d7289267296979be30c599e2cca98d33c1dca
Edison Panchi <edison@cladian.com>
2022-01-21 21:55:55 -0500

    OnChange Seach integration

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,13 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+  const onSearchValueChange= (e)=>{
+    console.log(e.target.value);
+  };
   return (
-    <input className="TodoSearch" placeholder="Cebolla" />
+    <input className="TodoSearch" placeholder="Buscar"
+    onChange={onSearchValueChange}
+    />
   );
 }
 
